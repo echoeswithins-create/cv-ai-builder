0363f1cd1eecf42b33e0ff2e16dfafa01bc36d05
echoeswithins-create <echoeswithins@gmail.com>
2025-09-29 16:44:25 +0300

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import BuilderPage from "./components/BuilderPage";
-import "./styles/builder.css";
-
-function App() {
-  return <BuilderPage />;
-}
-
-export default App;
