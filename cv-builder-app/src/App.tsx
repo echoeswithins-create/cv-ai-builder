c62acdced8ca1249ef97c49d86a48ae39564213b
echoeswithins-create <echoeswithins@gmail.com>
2025-10-01 14:56:14 +0300

    Add files via upload

---
+++ cv-builder-app/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import BuilderPage from "./components/BuilderPage";
+import "./styles/builder.css";
+
+function App() {
+  return <BuilderPage />;
+}
+
+export default App;
---
+++ cv-builder-app/src/components/BuilderPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BuilderPage() {
+  return (
+    <div>
+      <h1>CV Builder</h1>
+      <p>Welcome to the CV Builder App!</p>
+    </div>
+  );
+}
+
+export default BuilderPage;
